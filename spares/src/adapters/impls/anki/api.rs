c394966b853448692f417abe5a50d20b93cf9d81
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2025-10-31 21:04:24 -0400

    refactor: separated anki adapter into multiple files

---
+++ spares/src/adapters/impls/anki/api.rs
@@ -0,0 +1,97 @@
+use crate::adapters::impls::anki::ANKI_ADAPTER_NAME;
+use crate::adapters::impls::anki::types::{
+    AddFieldToModelApiRequestData, ApiAction, ApiRequest, ApiRequestParams, ModelName,
+};
+use crate::{AdapterErrorKind, Error, LibraryError};
+use indicatif::ProgressIterator;
+use reqwest::Client;
+use serde_json::Value;
+
+pub async fn execute_request(request: &ApiRequest, client: &Client) -> Result<Value, Error> {
+    let api_url = "http://localhost:8765";
+    let body = serde_json::to_string_pretty(&request).map_err(|e| {
+        Error::Library(LibraryError::Adapter(AdapterErrorKind::Custom {
+            adapter_name: ANKI_ADAPTER_NAME.to_string(),
+            error: e.to_string(),
+        }))
+    })?;
+    // println!("{}", serde_json::to_string_pretty(&request).unwrap());
+    let response = client.post(api_url).body(body).send().await.map_err(|e| {
+        Error::Library(LibraryError::Adapter(AdapterErrorKind::Custom {
+            adapter_name: ANKI_ADAPTER_NAME.to_string(),
+            error: format!("Failed to send the API request: {}", e),
+        }))
+    })?;
+    if response.status().is_success() {
+        let response_value = response.json::<Value>().await.map_err(|e| {
+            Error::Library(LibraryError::Adapter(AdapterErrorKind::Custom {
+                adapter_name: ANKI_ADAPTER_NAME.to_string(),
+                error: format!("Failed to get response body: {}", e),
+            }))
+        })?;
+        // <https://git.foosoft.net/alex/anki-connect#sample-invocation>
+        let response_result = response_value.get("result");
+        if let Some(response) = response_result {
+            return Ok(response.clone());
+        }
+        let response_error =
+            response_value
+                .get("error")
+                .ok_or(Error::Library(LibraryError::Adapter(
+                    AdapterErrorKind::Custom {
+                        adapter_name: ANKI_ADAPTER_NAME.to_string(),
+                        error: "Failed to get 'result'".to_string(),
+                    },
+                )))?;
+        Err(Error::Library(LibraryError::Adapter(
+            AdapterErrorKind::Custom {
+                adapter_name: ANKI_ADAPTER_NAME.to_string(),
+                error: format!(
+                    "Failed to get 'result'. Got an 'error' of: {}",
+                    response_error
+                ),
+            },
+        )))
+    } else {
+        Err(Error::Library(LibraryError::Adapter(
+            AdapterErrorKind::Custom {
+                adapter_name: ANKI_ADAPTER_NAME.to_string(),
+                error: format!("Request failed with status code: {}", response.status()),
+            },
+        )))
+    }
+}
+
+pub async fn execute_requests(
+    requests: &[ApiRequest],
+    run: bool,
+    quiet: bool,
+    client: &Client,
+) -> Result<Vec<Value>, Error> {
+    let mut results = Vec::new();
+    for (i, request) in requests.iter().enumerate().progress() {
+        if run {
+            let result = execute_request(request, client).await?;
+            if !quiet {
+                println!("{}: {}", i, result);
+            }
+            results.push(result);
+        }
+    }
+    Ok(results)
+}
+
+pub async fn create_field(field_name: &str, client: &Client) -> Result<(), Error> {
+    let params = ApiRequestParams::AddFieldToModel(AddFieldToModelApiRequestData {
+        model_name: ModelName::Basic,
+        field_name: field_name.to_string(),
+        index: None,
+    });
+    let api_request = ApiRequest {
+        action: ApiAction::GetModelFieldNames,
+        params,
+        version: 6,
+    };
+    let _response = execute_request(&api_request, client).await?;
+    Ok(())
+}
---
+++ spares/src/adapters/impls/anki/types.rs
@@ -0,0 +1,177 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use sqlx::FromRow;
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct DbNoteRow {
+    pub id: i64,
+    pub flds: String,
+    pub tags: String,
+}
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct DbCardRow {
+    pub id: i64,
+    pub queue: i64,
+    pub r#type: i64,
+    pub due: i64,
+    pub data: Value,
+}
+
+#[derive(Debug, Deserialize, FromRow, Serialize)]
+pub struct DbRevLogRow {
+    pub id: i64,     // reviewed_at, but this is in milliseconds
+    pub ease: i64,   // rating
+    pub r#type: i64, // new state of the card
+    pub ivl: i64,    // scheduled_time
+    pub time: i64,   // duration, but this is in milliseconds
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub enum ModelName {
+    #[serde(rename = "Basic")]
+    Basic,
+    #[serde(rename = "Basic (and reversed card)")]
+    BasicAndReversed,
+    #[serde(rename = "Cloze")]
+    Cloze,
+}
+
+// API
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ApiRequest {
+    pub action: ApiAction,
+    pub params: ApiRequestParams,
+    pub version: u32, // 6
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum ApiAction {
+    #[serde(rename = "addNote")]
+    AddNote,
+    #[serde(rename = "updateNote")]
+    UpdateNote,
+    #[serde(rename = "deleteNotes")]
+    DeleteNote,
+    #[serde(rename = "guiBrowse")]
+    GuiBrowse,
+    #[serde(rename = "findCards")]
+    FindCards,
+    #[serde(rename = "suspend")]
+    Suspend,
+    #[serde(rename = "modelFieldNames")]
+    GetModelFieldNames,
+    #[serde(rename = "modelFieldAdd")]
+    AddFieldToModel,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum ApiRequestParams {
+    AddNote(AddNoteApiRequestData),
+    UpdateNote(UpdateNoteApiRequestData),
+    DeleteNote(DeleteNoteApiRequestData),
+    GuiBrowse(GuiBrowseApiRequestData),
+    FindCards(FindCardsApiRequestData),
+    Suspend(SuspendApiRequestData),
+    GetModelFieldNames(GetModelFieldNamesApiRequestData),
+    AddFieldToModel(AddFieldToModelApiRequestData),
+}
+
+// General Note Fields
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct NoteFields {
+    #[serde(rename = "Front", skip_serializing_if = "Option::is_none")]
+    pub front: Option<String>,
+    #[serde(rename = "Back", skip_serializing_if = "Option::is_none")]
+    pub back: Option<String>,
+    #[serde(rename = "Keywords", skip_serializing_if = "Option::is_none")]
+    pub keywords: Option<String>,
+    #[serde(rename = "SparesId", skip_serializing_if = "Option::is_none")]
+    pub spares_id: Option<String>,
+    #[serde(rename = "SparesParserName", skip_serializing_if = "Option::is_none")]
+    pub spares_parser_name: Option<String>,
+}
+
+// Add Note
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AddNoteApiRequestData {
+    pub note: AddNoteApiRequestNoteData,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AddNoteApiRequestNoteData {
+    #[serde(rename = "deckName")]
+    pub deck_name: String,
+    #[serde(rename = "modelName")]
+    pub model_name: ModelName,
+    pub fields: NoteFields,
+    pub tags: Vec<String>,
+    pub options: AddNoteApiRequestOptions,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub struct AddNoteApiRequestOptions {
+    #[serde(rename = "allowDuplicate")]
+    pub allow_duplicate: bool, // True
+}
+
+// Update Note Fields
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct UpdateNoteApiRequestData {
+    pub note: UpdateNoteApiRequestNoteData,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct UpdateNoteApiRequestNoteData {
+    #[serde(rename = "deckName")]
+    pub deck_name: String,
+    #[serde(rename = "modelName")]
+    pub model_name: ModelName,
+    pub id: i64,
+    pub fields: NoteFields,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tags: Option<Vec<String>>,
+}
+
+// Delete Note
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct DeleteNoteApiRequestData {
+    pub notes: Vec<i64>,
+}
+
+// Gui Browse
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GuiBrowseApiRequestData {
+    pub query: String,
+}
+
+// Find Cards
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct FindCardsApiRequestData {
+    pub query: String,
+}
+
+// Suspend
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SuspendApiRequestData {
+    pub cards: Vec<i64>,
+}
+
+// Get model field names
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetModelFieldNamesApiRequestData {
+    #[serde(rename = "modelName")]
+    pub model_name: ModelName,
+}
+
+// Add field to model
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AddFieldToModelApiRequestData {
+    #[serde(rename = "modelName")]
+    pub model_name: ModelName,
+    #[serde(rename = "fieldName")]
+    pub field_name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub index: Option<usize>,
+}
