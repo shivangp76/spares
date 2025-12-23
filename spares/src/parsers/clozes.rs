75f36af8ee6377be05ae4278c5d446884ddaf849
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2025-12-23 12:23:21 -0500

    fix: change default back emphasis

--- spares/src/parsers/clozes.rs
+++ spares/src/parsers/clozes.rs
@@ -9,7 +9,11 @@ use serde::{Deserialize, Serialize};
 use std::ops::Range;
 use std::str::FromStr;
 
+#[cfg(test)]
 pub const DEFAULT_BACK_EMPHASIS: bool = false;
+
+#[cfg(not(test))]
+pub const DEFAULT_BACK_EMPHASIS: bool = true;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ClozeMatch {
