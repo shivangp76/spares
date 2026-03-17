51d2ba368eb66c3b95c57b5f8e25a2fbc16a6c17
Shivang Patel <30023891+shivangp76@users.noreply.github.com>
2026-03-16 22:08:16 -0400

    feat: added undo functionality to tag methods

--- spares_server/src/handlers/tag.rs
+++ spares_server/src/handlers/tag.rs
@@ -20,7 +20,7 @@ pub async fn create_tag_handler(
     axum::extract::State(data): axum::extract::State<Arc<AppState>>,
     Json(body): Json<CreateTagRequest>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let result = create_tag(&data.db, body)
+    let result = create_tag(&data.db, body, true)
         .await
         .map_err(error_to_response)?;
     Ok(Json(result))
@@ -48,7 +48,7 @@ pub async fn update_tag_handler(
     axum::extract::State(data): axum::extract::State<Arc<AppState>>,
     Json(body): Json<UpdateTagRequest>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let update_tag_res = update_tag(&data.db, body)
+    let update_tag_res = update_tag(&data.db, body, true)
         .await
         .map_err(error_to_response)?;
     Ok(Json(update_tag_res))
@@ -58,7 +58,7 @@ pub async fn delete_tag_handler(
     Path(id): Path<i64>,
     axum::extract::State(data): axum::extract::State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    delete_tag(&data.db, id).await.map_err(error_to_response)?;
+    delete_tag(&data.db, id, true).await.map_err(error_to_response)?;
     Ok(StatusCode::OK)
 }
 
